9b7c4677a9166cb09e9d2381b0c0c4508cd543b5
gilmarkandrade <45281863+gilmarkandrade@users.noreply.github.com>
2020-02-01 14:15:59 -0300

    update tes engine

--- game_in_engine/script/script_interact/engine.gd
+++ game_in_engine/script/script_interact/engine.gd
@@ -1,31 +1,27 @@
 extends Area2D
 
-export var emiter_value = {"signal1":false,"signal2":false,"signal3":false}
+export var emiter_value = {"signal1":false,"signal2":false,"signal3":false,"signal4":false,"signal5":false,"signal6":false}
 var it_is_key = true
 var activate_engine = false
 var it_is_in_area = false
 var signal_emited = false   
 
-func _ready(): 
-	machine_singleton.emitter_value.sinal1 = emiter_value.signal1
-	machine_singleton.emitter_value.sinal2 = emiter_value.signal2
-	machine_singleton.emitter_value.sinal3 = emiter_value.signal3
+
 func _process(delta):
 	
 	if Input.is_action_just_pressed("ui_accept"):
 		if it_is_in_area == true :
 			activate_engine = true
 			emiter_signal()
-		if it_is_in_area == false :
-			activate_engine = false
-
+		
+			
+	if Input.is_action_just_released("ui_accept"):
+		activate_engine = false
 	
 
 func emiter_signal():
 	
-	machine_singleton.emitter_value.sinal1 = emiter_value.signal1
-	machine_singleton.emitter_value.sinal2 = emiter_value.signal2
-	machine_singleton.emitter_value.sinal3 = emiter_value.signal3
+
 	if activate_engine == true :
 		if emiter_value.signal1 == true  and machine_singleton.key_in_hand >= 1:
 			if activate_engine == true :
@@ -45,7 +41,26 @@ func emiter_signal():
 				signal_emited = true
 				machine_singleton.emited_signal_engine = signal_emited
 				machine_singleton.key_in_hand -= 1
-
+				
+		if emiter_value.signal4 == true and machine_singleton.key_in_hand >= 1:
+			if activate_engine == true :
+				signal_emited = true
+				machine_singleton.emited_signal_engine = signal_emited
+				machine_singleton.key_in_hand -= 1
+				
+		if emiter_value.signal5 == true and machine_singleton.key_in_hand >= 1:
+			if activate_engine == true :
+				signal_emited = true
+				machine_singleton.emited_signal_engine = signal_emited
+				machine_singleton.key_in_hand -= 1
+		
+		if emiter_value.signal6 == true and machine_singleton.key_in_hand >= 1:
+			if activate_engine == true :
+				signal_emited = true
+				machine_singleton.emited_signal_engine = signal_emited
+				machine_singleton.key_in_hand -= 1
+				
+				
 func _on_engine_body_entered(body):
 	
 	print(emiter_value)
--- game_in_engine/script/script_interact/receiver_signal.gd
+++ game_in_engine/script/script_interact/receiver_signal.gd
@@ -2,30 +2,35 @@ extends Node2D
 export var receiver_channel = 0 
 export var name_animation = ""
 export var animation_pause = false
-var pauseanimation = false
+var pauseanimation = {"chanel1":false,"chanel2":false,"chanel3":false,"chanel4":false,"chanel5":false,"chanel6":false}
+var stop_check = false
 
 func check_signal_emited():
 	
 	
-	if machine_singleton.emitter_value.sinal1 == true and machine_singleton.emited_signal_engine == true  and receiver_channel == 1 and pauseanimation == false:
+	if machine_singleton.emitter_value.sinal1 == true and machine_singleton.emited_signal_engine == true  and receiver_channel == 1 and pauseanimation.chanel1 == false:
 		$AnimationPlayer.current_animation =""+str(name_animation)+""
-		if animation_pause == true:
-			pauseanimation = true
-		else:
-			pauseanimation = false
-	if machine_singleton.emitter_value.sinal2 == true and machine_singleton.emited_signal_engine == true and receiver_channel == 2 and pauseanimation == false:
-		$AnimationPlayer2.current_animation =""+str(name_animation)+""
-		if animation_pause == true:
-			pauseanimation = true
-		else:
-			pauseanimation = false
-	if machine_singleton.emitter_value.sinal3 == true  and machine_singleton.emited_signal_engine == true and receiver_channel == 3 and pauseanimation == false:
-		$AnimationPlayer3.current_animation =""+str(name_animation)+""
-		if animation_pause == true:
-			pauseanimation = true
-		else:
-			pauseanimation = false
+		#if animation_pause == true:
+		pauseanimation.chanel1 = true
+		#else:
+		#	pauseanimation = false
+	if machine_singleton.emitter_value.sinal2 == true and machine_singleton.emited_signal_engine == true and receiver_channel == 2 and pauseanimation.chanel2 == false:
+		$AnimationPlayer.current_animation =""+str(name_animation)+""
+		pauseanimation.chanel2 = true
+	if machine_singleton.emitter_value.sinal3 == true  and machine_singleton.emited_signal_engine == true and receiver_channel == 3 and pauseanimation.chanel3 == false:
+		$AnimationPlayer.current_animation =""+str(name_animation)+""
+		pauseanimation.chanel3 = true
 		
-
+	if machine_singleton.emitter_value.sinal4 == true  and machine_singleton.emited_signal_engine == true and receiver_channel == 4 and pauseanimation.chanel4 == false:
+		$AnimationPlayer.current_animation =""+str(name_animation)+""
+		pauseanimation.chanel4 = true
+		
+	if machine_singleton.emitter_value.sinal5 == true  and machine_singleton.emited_signal_engine == true and receiver_channel == 5 and pauseanimation.chanel5 == false:
+		$AnimationPlayer.current_animation =""+str(name_animation)+""
+		pauseanimation.chanel5 = true
+	if machine_singleton.emitter_value.sinal6 == true  and machine_singleton.emited_signal_engine == true and receiver_channel == 6 and pauseanimation.chanel6 == false:
+		$AnimationPlayer.current_animation =""+str(name_animation)+""
+		pauseanimation.chanel6 = true
+		
 func _on_check_status_timeout():
 	check_signal_emited()
