7d2242bcafc6270e0fdd4ed4f11d82170d01673b
gilmarkandrade <45281863+gilmarkandrade@users.noreply.github.com>
2020-02-01 14:58:24 -0300

    test engine

--- game_in_engine/script/script_interact/portal.gd
+++ game_in_engine/script/script_interact/portal.gd
@@ -8,4 +8,10 @@ export var next_scene = 0
 func _on_portal_body_entered(body):
 	var path = "res://levels/"+str(next_scene)+".tscn"
 	if body.is_in_group("player"):
+		machine_singleton.emitter_value.sinal1= false
+		machine_singleton.emitter_value.sinal2 = false
+		machine_singleton.emitter_value.sinal3= false
+		machine_singleton.emitter_value.sinal4 = false
+		machine_singleton.emitter_value.sinal5=false
+		machine_singleton.emitter_value.sinal6 = false
 		get_tree().change_scene(path)
