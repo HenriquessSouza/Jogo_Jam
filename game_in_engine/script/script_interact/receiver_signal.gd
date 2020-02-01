dcd5ac53621107ded6b42ce94d738db3df2bd771
gilmarkandrade <45281863+gilmarkandrade@users.noreply.github.com>
2020-02-01 10:35:23 -0300

    update receiver

--- game_in_engine/script/script_interact/receiver_signal.gd
+++ game_in_engine/script/script_interact/receiver_signal.gd
@@ -1,20 +1,31 @@
 extends Node2D
 export var receiver_channel = 0 
 export var name_animation = ""
+export var animation_pause = false
+var pauseanimation = false
 
 func check_signal_emited():
 	
 	
-	if machine_singleton.emitter_value.sinal1 == true and machine_singleton.emited_signal_engine == true  and receiver_channel == 1:
+	if machine_singleton.emitter_value.sinal1 == true and machine_singleton.emited_signal_engine == true  and receiver_channel == 1 and pauseanimation == false:
 		$AnimationPlayer.play(""+str(name_animation)+"")
-		print(1)
-	if machine_singleton.emitter_value.sinal2 == true and machine_singleton.emited_signal_engine == true and receiver_channel == 2:
+		if animation_pause == true:
+			pauseanimation = true
+		else:
+			pauseanimation = false
+	if machine_singleton.emitter_value.sinal2 == true and machine_singleton.emited_signal_engine == true and receiver_channel == 2 and pauseanimation == false:
 		$AnimationPlayer.play(""+str(name_animation)+"")
-		print(2)
-	if machine_singleton.emitter_value.sinal3 == true  and machine_singleton.emited_signal_engine == true and receiver_channel == 3:
+		if animation_pause == true:
+			pauseanimation = true
+		else:
+			pauseanimation = false
+	if machine_singleton.emitter_value.sinal3 == true  and machine_singleton.emited_signal_engine == true and receiver_channel == 3 and pauseanimation == false:
 		$AnimationPlayer.play(""+str(name_animation)+"")
-		print(3)
-
+		if animation_pause == true:
+			pauseanimation = true
+		else:
+			pauseanimation = false
+		
 
 func _on_check_status_timeout():
 	check_signal_emited()
