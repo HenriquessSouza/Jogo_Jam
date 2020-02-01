a0f5494485fe39de5c6cbb376439905635675d4d
gilmarkandrade <45281863+gilmarkandrade@users.noreply.github.com>
2020-02-01 11:47:23 -0300

    updTE ENGINE

--- game_in_engine/script/script_interact/engine.gd
+++ game_in_engine/script/script_interact/engine.gd
@@ -16,7 +16,7 @@ func _process(delta):
 		if it_is_in_area == true :
 			activate_engine = true
 			emiter_signal()
-		else :
+		if it_is_in_area == false :
 			activate_engine = false
 
 	
@@ -26,23 +26,23 @@ func emiter_signal():
 	machine_singleton.emitter_value.sinal1 = emiter_value.signal1
 	machine_singleton.emitter_value.sinal2 = emiter_value.signal2
 	machine_singleton.emitter_value.sinal3 = emiter_value.signal3
-	
-	if emiter_value.signal1 == true  and machine_singleton.key_in_hand >= 1:
-		if activate_engine == true :
-			signal_emited = true
-			machine_singleton.emited_signal_engine = signal_emited
-			machine_singleton.key_in_hand -= 1
-	
-	if emiter_value.signal2 ==true and machine_singleton.key_in_hand >= 1:
-		if activate_engine == true :
-			signal_emited = true
-			machine_singleton.emited_signal_engine = signal_emited
-			machine_singleton.key_in_hand -= 1
-	if emiter_value.signal3 == true and machine_singleton.key_in_hand >= 1:
-		if activate_engine == true :
-			signal_emited = true
-			machine_singleton.emited_signal_engine = signal_emited
-			machine_singleton.key_in_hand -= 1
+	if activate_engine == true :
+		if emiter_value.signal1 == true  and machine_singleton.key_in_hand >= 1:
+			if activate_engine == true :
+				signal_emited = true
+				machine_singleton.emited_signal_engine = signal_emited
+				machine_singleton.key_in_hand -= 1
+		
+		if emiter_value.signal2 ==true and machine_singleton.key_in_hand >= 1:
+			if activate_engine == true :
+				signal_emited = true
+				machine_singleton.emited_signal_engine = signal_emited
+				machine_singleton.key_in_hand -= 1
+		if emiter_value.signal3 == true and machine_singleton.key_in_hand >= 1:
+			if activate_engine == true :
+				signal_emited = true
+				machine_singleton.emited_signal_engine = signal_emited
+				machine_singleton.key_in_hand -= 1
 
 func _on_engine_body_entered(body):
 	if body.is_in_group("player"):
@@ -52,3 +52,4 @@ func _on_engine_body_entered(body):
 func _on_engine_body_exited(body):
 	if body.is_in_group("player"):
 		it_is_in_area = true
+		signal_emited = false
