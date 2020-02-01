ea8360619865e7a54ebd20c07b6ae42fc68273ab
gilmarkandrade <45281863+gilmarkandrade@users.noreply.github.com>
2020-02-01 16:17:00 -0300

    Update receiver_signal.gd

--- game_in_engine/script/script_interact/receiver_signal.gd
+++ game_in_engine/script/script_interact/receiver_signal.gd
@@ -7,38 +7,36 @@ var stop_check = false
 
 func check_signal_emited():
 	
-	if  machine_singleton.key_in_hand >= 1 :
+
 		
-		
-		
-		if machine_singleton.emitter_value.sinal1 == true and machine_singleton.emited_signal_engine == true  and receiver_channel == 1 and pauseanimation.chanel1 == false :
-			$AnimationPlayer.current_animation =""+str(name_animation)+""
-			#if animation_pause == true:
-			pauseanimation.chanel1 = true
+	if machine_singleton.emitter_value.sinal1 == true and machine_singleton.emited_signal_engine == true  and receiver_channel == 1 and pauseanimation.chanel1 == false :
+		$AnimationPlayer.current_animation =""+str(name_animation)+""
+	#if animation_pause == true:
+		pauseanimation.chanel1 = true
 			
 			#else:
 			#	pauseanimation = false
-		if machine_singleton.emitter_value.sinal2 == true and machine_singleton.emited_signal_engine == true and receiver_channel == 2 and pauseanimation.chanel2 == false :
-			$AnimationPlayer.current_animation =""+str(name_animation)+""
-			pauseanimation.chanel2 = true
+	if machine_singleton.emitter_value.sinal2 == true and machine_singleton.emited_signal_engine == true and receiver_channel == 2 and pauseanimation.chanel2 == false :
+		$AnimationPlayer.current_animation =""+str(name_animation)+""
+		pauseanimation.chanel2 = true
 			
-		if machine_singleton.emitter_value.sinal3 == true  and machine_singleton.emited_signal_engine == true and receiver_channel == 3 and pauseanimation.chanel3 == false :
-			$AnimationPlayer.current_animation =""+str(name_animation)+""
-			pauseanimation.chanel3 = true
-			
-			
-		if machine_singleton.emitter_value.sinal4 == true  and machine_singleton.emited_signal_engine == true and receiver_channel == 4 and pauseanimation.chanel4 == false :
-			$AnimationPlayer.current_animation =""+str(name_animation)+""
-			pauseanimation.chanel4 = true
+	if machine_singleton.emitter_value.sinal3 == true  and machine_singleton.emited_signal_engine == true and receiver_channel == 3 and pauseanimation.chanel3 == false :
+		$AnimationPlayer.current_animation =""+str(name_animation)+""
+		pauseanimation.chanel3 = true
 			
 			
-		if machine_singleton.emitter_value.sinal5 == true  and machine_singleton.emited_signal_engine == true and receiver_channel == 5 and pauseanimation.chanel5 == false :
-			$AnimationPlayer.current_animation =""+str(name_animation)+""
-			pauseanimation.chanel5 = true
+	if machine_singleton.emitter_value.sinal4 == true  and machine_singleton.emited_signal_engine == true and receiver_channel == 4 and pauseanimation.chanel4 == false :
+		$AnimationPlayer.current_animation =""+str(name_animation)+""
+		pauseanimation.chanel4 = true
 			
-		if machine_singleton.emitter_value.sinal6 == true  and machine_singleton.emited_signal_engine == true and receiver_channel == 6 and pauseanimation.chanel6 == false :
-			$AnimationPlayer.current_animation =""+str(name_animation)+""
-			pauseanimation.chanel6 = true
+			
+	if machine_singleton.emitter_value.sinal5 == true  and machine_singleton.emited_signal_engine == true and receiver_channel == 5 and pauseanimation.chanel5 == false :
+		$AnimationPlayer.current_animation =""+str(name_animation)+""
+		pauseanimation.chanel5 = true
+			
+	if machine_singleton.emitter_value.sinal6 == true  and machine_singleton.emited_signal_engine == true and receiver_channel == 6 and pauseanimation.chanel6 == false :
+		$AnimationPlayer.current_animation =""+str(name_animation)+""
+		pauseanimation.chanel6 = true
 		
 		
 		
